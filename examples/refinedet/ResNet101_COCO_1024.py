554daf302b4b43680126f8ff544e38f88d611509
Troywwww <wjltroy@gmail.com>
2018-11-27 16:50:42 +0800

    test speed

--- examples/refinedet/ResNet101_COCO_1024.py
+++ examples/refinedet/ResNet101_COCO_1024.py
@@ -311,13 +311,13 @@ clip = False
 
 # Solver parameters.
 # Defining which GPUs to use.
-gpus = "2,3,4,5,6,7"
+gpus = "0"
 gpulist = gpus.split(",")
 num_gpus = len(gpulist)
 
 # Divide the mini-batch to different GPUs.
-batch_size = 18
-accum_batch_size = 18
+batch_size = 2
+accum_batch_size = 2
 iter_size = accum_batch_size / batch_size
 solver_mode = P.Solver.CPU
 device_id = 0
