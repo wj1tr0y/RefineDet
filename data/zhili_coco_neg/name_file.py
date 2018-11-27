58377e67464ad4b1d8f96df3ebbd570517d69b92
Troywwww <wjltroy@gmail.com>
2018-11-27 14:34:05 +0800

    add dataset name file generation script

---
+++ data/zhili_coco_neg/name_file.py
@@ -0,0 +1,10 @@
+import os
+HOMEDIR = os.path.expanduser("~")
+DATASETDIR = os.path.join(HOMEDIR, 'data/neg/')
+
+im_names = os.listdir(os.path.join(DATASETDIR, 'Annotations'))
+im_names = [x[:-5] for x in im_names]
+im_names.sort()
+
+with open(os.path.join(DATASETDIR,'ImageSet/zhili_coco_neg.txt'), 'w') as f:
+    f.write("\n".join(im_names))
