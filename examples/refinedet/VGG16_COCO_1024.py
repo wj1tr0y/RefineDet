baad2583f60ff7f7644c36d4d5a7f483b172eb7f
Troywwww <wjltroy@gmail.com>
2018-11-28 15:38:05 +0800

    change gpus and batch_size

--- examples/refinedet/VGG16_COCO_1024.py
+++ examples/refinedet/VGG16_COCO_1024.py
@@ -339,8 +339,8 @@ gpulist = gpus.split(",")
 num_gpus = len(gpulist)
 
 # Divide the mini-batch to different GPUs.
-batch_size = 10
-accum_batch_size = 10
+batch_size = 8
+accum_batch_size = 8
 iter_size = accum_batch_size / batch_size
 solver_mode = P.Solver.CPU
 device_id = 0
