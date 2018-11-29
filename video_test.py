d9193236d8db85a69bed504bd2c449cc261b84af
Troywwww <wjltroy@gmail.com>
2018-11-29 14:51:45 +0800

    add video test

--- video_test.py
+++ video_test.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
 
     cap.release()
 
-    cmd = "python run_test.py --gpuid 0 --out-dir result --test-set videocap"
+    cmd = "python run_test.py --gpuid {} --out-dir result --test-set videocap".format(args.gpuid)
     print(cmd)
     process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
