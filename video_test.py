09c026f0f4ea266117f0aefaf1fe178779e4792d
Troywwww <wjltroy@gmail.com>
2018-11-30 11:05:04 +0800

    adjust test model file and fix typo

--- video_test.py
+++ video_test.py
@@ -3,6 +3,8 @@ import argparse
 import os
 import shutil
 import subprocess
+import zipfile
+import time
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = "run test and get all result")
@@ -19,7 +21,7 @@ if __name__ == '__main__':
     if not os.path.exists(video_name):
         print "{} doesn't exist.".format(video_name)
 
-    frame_save_dir = '../dataset/test/videocap'
+    frame_save_dir = '../dataset/test/videocap'+ str(int(time.time()))
     if os.path.exists(frame_save_dir):
         shutil.rmtree(frame_save_dir)
         os.mkdir(frame_save_dir)
@@ -27,6 +29,9 @@ if __name__ == '__main__':
         os.mkdir(frame_save_dir)
     
     cap = cv2.VideoCapture(video_name)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
     frame_count = 1
     success = True
     while(success):
@@ -39,9 +44,24 @@ if __name__ == '__main__':
             print ''
     cap.release()
 
+    if os.path.exists('result'):
+        shutil.rmtree('result')
+        os.mkdir('result')
+
     print 'Detecting pedestrian.....'
-    cmd = "python run_test.py --gpuid {} --out-dir result --test-set videocap".format(args.gpus)
+    cmd = "python run_test.py --gpuid {} --out-dir result --test-set videocap".format(args.gpuid)
     print(cmd)
     process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     output = process.communicate()
     print(output)
+
+    videoWriter = cv2.VideoWriter('result.avi', cv2.VideoWriter_fourcc('X','V','I','D'), fps, size)
+    frame_name = os.listdir('result')
+    frame_name = sorted(frame_name, key=lambda x: int(x[5:-9]))
+    for i in frame_name:
+        frame = cv2.imread('result/' + i)
+        videoWriter.write(frame)
+    videoWriter.release()
+
+
+    shutil.rmtree(frame_save_dir)
