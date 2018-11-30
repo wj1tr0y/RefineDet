401a12ec6cbee57c646b507a633aedf7eb5bf404
Troywwww <wjltroy@gmail.com>
2018-11-30 11:04:25 +0800

    adjust test model file and fix typo

--- video_test.py
+++ video_test.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     if not os.path.exists(video_name):
         print "{} doesn't exist.".format(video_name)
 
-    frame_save_dir = '../dataset/test/videocap'
+    frame_save_dir = '../dataset/test/videocap'+ str(int(time.time()))
     if os.path.exists(frame_save_dir):
         shutil.rmtree(frame_save_dir)
         os.mkdir(frame_save_dir)
@@ -61,3 +61,6 @@ if __name__ == '__main__':
         frame = cv2.imread('result/' + i)
         videoWriter.write(frame)
     videoWriter.release()
+
+
+    shutil.rmtree(frame_save_dir)
