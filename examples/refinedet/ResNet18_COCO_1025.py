e49e58a76df18c60f8f47c3676374e43f9ffeac5
Troywwww <wjltroy@gmail.com>
2018-12-06 17:12:26 +0800

    change datalayers

--- examples/refinedet/ResNet18_COCO_1025.py
+++ examples/refinedet/ResNet18_COCO_1025.py
@@ -404,7 +404,6 @@ net = caffe.NetSpec()
 net.data, net.label = CreateAnnotatedDataLayer(train_data, batch_size=batch_size_per_device,
         train=True, output_label=True, label_map_file=label_map_file,
         transform_param=train_transform_param, batch_sampler=batch_sampler)
-print(type(net.data))
-ResNet18Body(net, from_layer='data', use_pool5=False, use_dilation_conv5=False)
 
-print(net.to_proto())
+for i in net.keys():
+        print(i)
