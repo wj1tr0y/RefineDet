2a3fa8f96bef4149ae8742233c67e75eb00faf85
Troywwww <wjltroy@gmail.com>
2018-12-06 19:12:41 +0800

    add new script

--- examples/refinedet/ResNet18_COCO_addneg_1024.py
+++ examples/refinedet/ResNet18_COCO_addneg_1024.py
@@ -317,8 +317,8 @@ gpulist = gpus.split(",")
 num_gpus = len(gpulist)
 
 # Divide the mini-batch to different GPUs.
-batch_size = 140
-accum_batch_size = 140 
+batch_size = 135
+accum_batch_size = 135 
 iter_size = accum_batch_size / batch_size
 solver_mode = P.Solver.CPU
 device_id = 0
