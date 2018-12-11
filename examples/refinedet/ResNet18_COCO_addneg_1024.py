6272fe6105a6bf2a2fd8e39bea35f4d2236b59dd
Troywwww <wjltroy@gmail.com>
2018-12-11 10:08:02 +0800

    change multisteps value

--- examples/refinedet/ResNet18_COCO_addneg_1024.py
+++ examples/refinedet/ResNet18_COCO_addneg_1024.py
@@ -317,8 +317,8 @@ gpulist = gpus.split(",")
 num_gpus = len(gpulist)
 
 # Divide the mini-batch to different GPUs.
-batch_size = 135
-accum_batch_size = 135 
+batch_size = 125
+accum_batch_size = 125 
 iter_size = accum_batch_size / batch_size
 solver_mode = P.Solver.CPU
 device_id = 0
