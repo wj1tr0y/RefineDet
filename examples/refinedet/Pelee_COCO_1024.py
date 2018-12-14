77ef5371ae5ca42994e508bd7f896d8b2b4d7077
Troywwww <wjltroy@gmail.com>
2018-12-14 18:15:31 +0800

    fix bugs

--- examples/refinedet/Pelee_COCO_1024.py
+++ examples/refinedet/Pelee_COCO_1024.py
@@ -308,8 +308,8 @@ gpulist = gpus.split(",")
 num_gpus = len(gpulist)
 
 # Divide the mini-batch to different GPUs.
-batch_size = 100
-accum_batch_size = 100
+batch_size = 25
+accum_batch_size = 25
 iter_size = accum_batch_size / batch_size
 solver_mode = P.Solver.CPU
 device_id = 0
