307b1f280dfbe130649f4363daaf1b4ab0f15cfd
Troywwww <wjltroy@gmail.com>
2018-12-16 15:24:06 +0800

    change batch_size

--- examples/refinedet/Pelee_COCO_512.py
+++ examples/refinedet/Pelee_COCO_512.py
@@ -308,8 +308,8 @@ gpulist = gpus.split(",")
 num_gpus = len(gpulist)
 
 # Divide the mini-batch to different GPUs.
-batch_size = 50
-accum_batch_size = 50
+batch_size = 70
+accum_batch_size = 70
 iter_size = accum_batch_size / batch_size
 solver_mode = P.Solver.CPU
 device_id = 0
