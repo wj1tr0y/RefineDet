7ddaa83a11a2798acf673e31fb023a6a6b9e99ad
Troywwww <wjltroy@gmail.com>
2018-12-19 17:32:01 +0800

    add hardexamples datasets

--- find_hard.py
+++ find_hard.py
@@ -101,12 +101,12 @@ if __name__ == "__main__":
                 for line in f.readlines():
                     hd.write(line)
     
-img_dir = '/home/wangjilong/data/zhili_coco_posneg/ImageSet'
-ann_dir = '/home/wangjilong/data/zhili_coco_posneg/Annotations'
-hard_dir = '/home/wangjilong/data/hardexamples'
-with open('hardexample.txt', 'r') as hd:
-    for i in hd.readlines():
-        shutil.copyfile(os.path.join(ann_dir, i[:-11] + '.json'), os.path.join(hard_dir, 'Annotations/'+i[:-11] + '.json'))
-        shutil.copyfile(os.path.join(img_dir, i[:-11] + '.jpg'), os.path.join(hard_dir, 'ImageSet/'+i[:-11] + '.jpg'))
-print('copy done')
+# img_dir = '/home/wangjilong/data/zhili_coco_posneg/ImageSet'
+# ann_dir = '/home/wangjilong/data/zhili_coco_posneg/Annotations'
+# hard_dir = '/home/wangjilong/data/hardexamples'
+# with open('hardexample.txt', 'r') as hd:
+#     for i in hd.readlines():
+#         shutil.copyfile(os.path.join(ann_dir, i[:-11] + '.json'), os.path.join(hard_dir, 'Annotations/'+i[:-11] + '.json'))
+#         shutil.copyfile(os.path.join(img_dir, i[:-11] + '.jpg'), os.path.join(hard_dir, 'ImageSet/'+i[:-11] + '.jpg'))
+# print('copy done')
 
