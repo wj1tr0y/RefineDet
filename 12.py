92affa72df20e9f7427cc3fd9f3c3d2f2a120f48
Troywwww <wjltroy@gmail.com>
2018-12-21 15:38:13 +0800

    find hardexamples

--- 12.py
+++ 12.py
@@ -4,15 +4,18 @@ img_dir = '/home/wangjilong/data/zhili_coco_posneg/ImageSet'
 ann_dir = '/home/wangjilong/data/zhili_coco_posneg/Annotations'
 de_ann_dir = '/home/wangjilong/data/hardexamples/Annotations'
 de_img_dir = '/home/wangjilong/data/hardexamples/ImageSet'
+fake_neg = '/home/wangjilong/data/neg/Annotations'
 with open('hardexample.txt', 'r') as hd:
     count = 0
+    fake = os.listdir(fake_neg)
     for i in hd.readlines():
         try:
-            a = int(i[:12])
+            int(i[:12])
         except:
-            shutil.copy(os.path.join(ann_dir, i[:-11] + '.json'), os.path.join(de_ann_dir, i[:-11] + '.json'))
-            shutil.copy(os.path.join(img_dir, i[:-11] + '.jpg'), os.path.join(de_img_dir, i[:-11] + '.jpg'))
-            count += 1
-            print(count)
+            if i not in fake:
+                shutil.copy(os.path.join(ann_dir, i[:-11] + '.json'), os.path.join(de_ann_dir, i[:-11] + '.json'))
+                shutil.copy(os.path.join(img_dir, i[:-11] + '.jpg'), os.path.join(de_img_dir, i[:-11] + '.jpg'))
+                count += 1
+                print(count)
      
 print('copy done')
