57d79e66efba298ffd45c6307c1822ae5177996f
Troywwww <wjltroy@gmail.com>
2018-12-21 15:06:42 +0800

    create hardexample dataset

--- 12.py
+++ 12.py
@@ -2,16 +2,17 @@ import shutil
 import os
 img_dir = '/home/wangjilong/data/zhili_coco_posneg/ImageSet'
 ann_dir = '/home/wangjilong/data/zhili_coco_posneg/Annotations'
-de_ann_dir = '/home/wangjilong/data/zhili_coco_posneg/COCO_useless_annotations'
+de_ann_dir = '/home/wangjilong/data/hardexamples/Annotations'
+de_img_dir = '/home/wangjilong/data/hardexamples/ImageSet'
 with open('hardexample.txt', 'r') as hd:
     count = 0
     for i in hd.readlines():
         try:
             a = int(i[:12])
+        except:
             shutil.move(os.path.join(ann_dir, i[:-11] + '.json'), os.path.join(de_ann_dir, i[:-11] + '.json'))
+            shutil.move(os.path.join(img_dir, i[:-11] + '.jpg'), os.path.join(de_img_dir, i[:-11] + '.jpg'))
             count += 1
             print(count)
-        except:
-            pass
      
 print('copy done')
