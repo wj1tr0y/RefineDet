1221927adaab2cc74f8f89ceea719e066bf4ce27
Troywwww <wjltroy@gmail.com>
2018-12-21 11:46:37 +0800

    change match algorithm

--- 12.py
+++ 12.py
@@ -2,9 +2,16 @@ import shutil
 import os
 img_dir = '/home/wangjilong/data/zhili_coco_posneg/ImageSet'
 ann_dir = '/home/wangjilong/data/zhili_coco_posneg/Annotations'
-hard_dir = '/home/wangjilong/data/hardexamples'
+de_ann_dir = '/home/wangjilong/data/zhili_coco_posneg/COCO_useless_annotations'
 with open('hardexample.txt', 'r') as hd:
+    count = 0
     for i in hd.readlines():
-        shutil.copyfile(os.path.join(ann_dir, i[:-11] + '.json'), os.path.join(hard_dir, 'Annotations/'+i[:-11] + '.json'))
-        shutil.copyfile(os.path.join(img_dir, i[:-11] + '.jpg'), os.path.join(hard_dir, 'ImageSet/'+i[:-11] + '.jpg'))
-print('copy done')
+        try:
+            a = int(i[:12])
+            count += 1
+        except:
+            pass
+    print(count)
+#     for i in hd.readlines():
+#         shutil.move(os.path.join(ann_dir, i[:-11] + '.json'), os.path.join(de_ann_dir, i[:-11] + '.json'))
+# print('copy done')
