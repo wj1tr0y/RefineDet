bd16b8afebaa92c6864366de9dda484e04cb5a5f
Troywwww <wjltroy@gmail.com>
2018-12-21 11:54:46 +0800

    change match algorithm

--- 12.py
+++ 12.py
@@ -8,10 +8,10 @@ with open('hardexample.txt', 'r') as hd:
     for i in hd.readlines():
         try:
             a = int(i[:12])
+            shutil.move(os.path.join(ann_dir, i[:-11] + '.json'), os.path.join(de_ann_dir, i[:-11] + '.json'))
             count += 1
+            print(count)
         except:
             pass
-    print(count)
-#     for i in hd.readlines():
-#         shutil.move(os.path.join(ann_dir, i[:-11] + '.json'), os.path.join(de_ann_dir, i[:-11] + '.json'))
-# print('copy done')
+     
+print('copy done')
