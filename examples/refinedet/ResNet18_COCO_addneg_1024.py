74c4697e0914490d6e647a9b50b4470b54702dc0
Troywwww <wjltroy@gmail.com>
2018-12-22 13:46:51 +0800

    add dual lmdb

--- examples/refinedet/ResNet18_COCO_addneg_1024.py
+++ examples/refinedet/ResNet18_COCO_addneg_1024.py
@@ -318,8 +318,8 @@ gpulist = gpus.split(",")
 num_gpus = len(gpulist)
 
 # Divide the mini-batch to different GPUs.
-batch_size = 108
-accum_batch_size = 108
+batch_size = 100
+accum_batch_size = 100
 iter_size = accum_batch_size / batch_size
 solver_mode = P.Solver.CPU
 device_id = 0
