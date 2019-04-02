8b71dec4af53bc44452be894ed600f29b1fa0de9
Troywwww <wjltroy@gmail.com>
2019-04-02 18:47:58 +0800

    add group conv

--- python/caffe/model_libs.py
+++ python/caffe/model_libs.py
@@ -6,7 +6,7 @@
 @Email: jilong.wang@watrix.ai
 @Description: file content
 @Date: 2019-03-15 15:04:39
-@LastEditTime: 2019-04-02 18:15:27
+@LastEditTime: 2019-04-02 18:47:22
 '''
 import os
 
@@ -177,6 +177,97 @@ def EltwiseLayer(net, from_layer, out_layer):
   elt_name = out_layer
   net[elt_name] = L.Eltwise(net[from_layer[0]], net[from_layer[1]])
 
+
+def DepthwiseConvBNLayer(net, from_layer, out_layer, use_bn, use_relu, num_output,
+    kernel_size, pad, stride, dilation=1, use_scale=True, lr_mult=1,
+    conv_prefix='', conv_postfix='', bn_prefix='', bn_postfix='_bn',
+    scale_prefix='', scale_postfix='_scale', bias_prefix='', bias_postfix='_bias',
+    **bn_params):
+  if use_bn:
+    # parameters for convolution layer with batchnorm. weight_filler=dict(type='xavier'), bias_filler=dict(type='constant')
+    kwargs = {
+        'param': [dict(lr_mult=lr_mult, decay_mult=1)],
+        'weight_filler': dict(type='xavier'),
+        'bias_term': False,
+        }
+    eps = bn_params.get('eps', 0.001)
+    moving_average_fraction = bn_params.get('moving_average_fraction', 0.999)
+    #moving_average_fraction = bn_params.get('moving_average_fraction', 0.1)
+    use_global_stats = bn_params.get('use_global_stats', False)
+    # parameters for batchnorm layer.
+    bn_kwargs = {
+        'param': [
+            dict(lr_mult=0, decay_mult=0),
+            dict(lr_mult=0, decay_mult=0),
+            dict(lr_mult=0, decay_mult=0)],
+        'eps': eps,
+        'moving_average_fraction': moving_average_fraction,
+        }
+    bn_lr_mult = lr_mult
+    if use_global_stats:
+      # only specify if use_global_stats is explicitly provided;
+      # otherwise, use_global_stats_ = this->phase_ == TEST;
+      bn_kwargs = {
+          'param': [
+              dict(lr_mult=0, decay_mult=0),
+              dict(lr_mult=0, decay_mult=0),
+              dict(lr_mult=0, decay_mult=0)],
+          'eps': eps,
+          'use_global_stats': use_global_stats,
+          }
+      # not updating scale/bias parameters
+      bn_lr_mult = 0
+    # parameters for scale bias layer after batchnorm.
+    if use_scale:
+      sb_kwargs = {
+          'bias_term': True,
+          'param': [
+              dict(lr_mult=bn_lr_mult, decay_mult=0),
+              dict(lr_mult=bn_lr_mult, decay_mult=0)],
+          'filler': dict(type='constant', value=1.0),
+          'bias_filler': dict(type='constant', value=0.0),
+          }
+    else:
+      bias_kwargs = {
+          'param': [dict(lr_mult=bn_lr_mult, decay_mult=0)],
+          'filler': dict(type='constant', value=0.0),
+          }
+  else:
+    kwargs = {
+        'param': [
+            dict(lr_mult=lr_mult, decay_mult=1),
+            dict(lr_mult=2 * lr_mult, decay_mult=0)],
+        'weight_filler': dict(type='xavier'),
+        'bias_filler': dict(type='constant', value=0)
+        }
+
+  conv_name = '{}{}{}'.format(conv_prefix, out_layer, conv_postfix)
+  [kernel_h, kernel_w] = UnpackVariable(kernel_size, 2)
+  [pad_h, pad_w] = UnpackVariable(pad, 2)
+  [stride_h, stride_w] = UnpackVariable(stride, 2)
+  if kernel_h == kernel_w:
+    net[conv_name] = L.Convolution(net[from_layer], num_output=num_output, group=num_output,
+        kernel_size=kernel_h, pad=pad_h, stride=stride_h, **kwargs)
+  else:
+    net[conv_name] = L.Convolution(net[from_layer], num_output=num_output,
+        kernel_h=kernel_h, kernel_w=kernel_w, pad_h=pad_h, pad_w=pad_w,
+        stride_h=stride_h, stride_w=stride_w, **kwargs)
+  if dilation > 1:
+    net.update(conv_name, {'dilation': dilation})
+  if use_bn:
+    bn_name = '{}{}{}'.format(bn_prefix, out_layer, bn_postfix)
+    net[bn_name] = L.BatchNorm(net[conv_name], in_place=True, **bn_kwargs)
+    if use_scale:
+      sb_name = '{}{}{}'.format(scale_prefix, out_layer, scale_postfix)
+      net[sb_name] = L.Scale(net[bn_name], in_place=True, **sb_kwargs)
+    else:
+      bias_name = '{}{}{}'.format(bias_prefix, out_layer, bias_postfix)
+      net[bias_name] = L.Bias(net[bn_name], in_place=True, **bias_kwargs)
+  if use_relu:
+    relu_name = '{}_relu'.format(conv_name)
+    net[relu_name] = L.ReLU(net[conv_name], in_place=True)
+
+
 def DepthwiseResidualBlock(net, from_layer, block_name, input_channel, out2a, out2b, stride, use_branch1, dilation=1, **bn_param):
     conv_prefix = 'res{}_'.format(block_name)
     conv_postfix = ''
@@ -184,11 +275,11 @@ def DepthwiseResidualBlock(net, from_layer, block_name, input_channel, out2a, ou
     bn_postfix = ''
     scale_prefix = 'scale{}_'.format(block_name)
     scale_postfix = ''
-    use_scale = False
+    use_scale = True
 
     if use_branch1:
         branch_name = 'branch1'
-        ConvBNLayer(net, from_layer, branch_name, use_bn=True, use_relu=False,
+        DepthwiseConvBNLayer(net, from_layer, branch_name, use_bn=True, use_relu=False,
             num_output=out2b, kernel_size=1, pad=0, stride=stride, use_scale=use_scale,
             conv_prefix=conv_prefix, conv_postfix=conv_postfix,
             bn_prefix=bn_prefix, bn_postfix=bn_postfix,
@@ -198,7 +289,7 @@ def DepthwiseResidualBlock(net, from_layer, block_name, input_channel, out2a, ou
         branch1 = from_layer
 
     branch_name = 'branch2a_3x3'
-    ConvBNLayer(net, from_layer, branch_name, use_bn=True, use_relu=True,
+    DepthwiseConvBNLayer(net, from_layer, branch_name, use_bn=True, use_relu=True,
         num_output=input_channel, kernel_size=3, pad=1, stride=stride, use_scale=use_scale,
         conv_prefix=conv_prefix, conv_postfix=conv_postfix,
         bn_prefix=bn_prefix, bn_postfix=bn_postfix,
@@ -207,7 +298,7 @@ def DepthwiseResidualBlock(net, from_layer, block_name, input_channel, out2a, ou
     out_name = '{}{}'.format(conv_prefix, branch_name)
 
     branch_name = 'branch2a'
-    ConvBNLayer(net, out_name, branch_name, use_bn=True, use_relu=True,
+    DepthwiseConvBNLayer(net, out_name, branch_name, use_bn=True, use_relu=True,
         num_output=out2a, kernel_size=1, pad=0, stride=1, use_scale=use_scale,
         conv_prefix=conv_prefix, conv_postfix=conv_postfix,
         bn_prefix=bn_prefix, bn_postfix=bn_postfix,
@@ -216,7 +307,7 @@ def DepthwiseResidualBlock(net, from_layer, block_name, input_channel, out2a, ou
     out_name = '{}{}'.format(conv_prefix, branch_name)
 
     branch_name = 'branch2b_3x3'
-    ConvBNLayer(net, out_name, branch_name, use_bn=True, use_relu=True,
+    DepthwiseConvBNLayer(net, out_name, branch_name, use_bn=True, use_relu=True,
         num_output=out2a, kernel_size=3, pad=1, stride=1, use_scale=use_scale,
         conv_prefix=conv_prefix, conv_postfix=conv_postfix,
         bn_prefix=bn_prefix, bn_postfix=bn_postfix,
@@ -225,7 +316,7 @@ def DepthwiseResidualBlock(net, from_layer, block_name, input_channel, out2a, ou
     out_name = '{}{}'.format(conv_prefix, branch_name)
 
     branch_name = 'branch2b'
-    ConvBNLayer(net, out_name, branch_name, use_bn=True, use_relu=True,
+    DepthwiseConvBNLayer(net, out_name, branch_name, use_bn=True, use_relu=True,
         num_output=out2b, kernel_size=1, pad=0, stride=1, use_scale=use_scale,
         conv_prefix=conv_prefix, conv_postfix=conv_postfix,
         bn_prefix=bn_prefix, bn_postfix=bn_postfix,
@@ -237,6 +328,66 @@ def DepthwiseResidualBlock(net, from_layer, block_name, input_channel, out2a, ou
     net[res_name] = L.Eltwise(net[branch1], net[branch2])
     relu_name = '{}_relu'.format(res_name)
     net[relu_name] = L.ReLU(net[res_name], in_place=True)
+
+def DepthwiseResBody(net, from_layer, block_name, input_channel, out2a, out2b, out2c, stride, use_branch1, dilation=1, **bn_param):
+  # ResBody(net, 'pool1', '2a', 64, 64, 256, 1, True)
+
+  conv_prefix = 'res{}_'.format(block_name)
+  conv_postfix = ''
+  bn_prefix = 'bn{}_'.format(block_name)
+  bn_postfix = ''
+  scale_prefix = 'scale{}_'.format(block_name)
+  scale_postfix = ''
+  use_scale = True
+
+  if use_branch1:
+    branch_name = 'branch1'
+    ConvBNLayer(net, from_layer, branch_name, use_bn=True, use_relu=False,
+        num_output=out2c, kernel_size=1, pad=0, stride=stride, use_scale=use_scale,
+        conv_prefix=conv_prefix, conv_postfix=conv_postfix,
+        bn_prefix=bn_prefix, bn_postfix=bn_postfix,
+        scale_prefix=scale_prefix, scale_postfix=scale_postfix, **bn_param)
+    branch1 = '{}{}'.format(conv_prefix, branch_name)
+  else:
+    branch1 = from_layer
+
+  branch_name = 'branch2a'
+  ConvBNLayer(net, from_layer, branch_name, use_bn=True, use_relu=True,
+      num_output=out2a, kernel_size=1, pad=0, stride=stride, use_scale=use_scale,
+      conv_prefix=conv_prefix, conv_postfix=conv_postfix,
+      bn_prefix=bn_prefix, bn_postfix=bn_postfix,
+      scale_prefix=scale_prefix, scale_postfix=scale_postfix, **bn_param)
+  out_name = '{}{}'.format(conv_prefix, branch_name)
+
+  branch_name = 'branch2b'
+  if dilation == 1:
+    ConvBNLayer(net, out_name, branch_name, use_bn=True, use_relu=True,
+        num_output=out2b, kernel_size=3, pad=1, stride=1, use_scale=use_scale,
+        conv_prefix=conv_prefix, conv_postfix=conv_postfix,
+        bn_prefix=bn_prefix, bn_postfix=bn_postfix,
+        scale_prefix=scale_prefix, scale_postfix=scale_postfix, **bn_param)
+  else:
+    pad = int((3 + (dilation - 1) * 2) - 1) / 2
+    ConvBNLayer(net, out_name, branch_name, use_bn=True, use_relu=True,
+        num_output=out2b, kernel_size=3, pad=pad, stride=1, use_scale=use_scale,
+        dilation=dilation, conv_prefix=conv_prefix, conv_postfix=conv_postfix,
+        bn_prefix=bn_prefix, bn_postfix=bn_postfix,
+        scale_prefix=scale_prefix, scale_postfix=scale_postfix, **bn_param)
+  out_name = '{}{}'.format(conv_prefix, branch_name)
+
+  branch_name = 'branch2c'
+  ConvBNLayer(net, out_name, branch_name, use_bn=True, use_relu=False,
+      num_output=out2c, kernel_size=1, pad=0, stride=1, use_scale=use_scale,
+      conv_prefix=conv_prefix, conv_postfix=conv_postfix,
+      bn_prefix=bn_prefix, bn_postfix=bn_postfix,
+      scale_prefix=scale_prefix, scale_postfix=scale_postfix, **bn_param)
+  branch2 = '{}{}'.format(conv_prefix, branch_name)
+
+  res_name = 'res{}'.format(block_name)
+  net[res_name] = L.Eltwise(net[branch1], net[branch2])
+  relu_name = '{}_relu'.format(res_name)
+  net[relu_name] = L.ReLU(net[res_name], in_place=True)
+
 
 
 # for resnet18
